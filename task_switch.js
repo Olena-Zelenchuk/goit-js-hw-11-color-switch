548dd7fc733a14880bc3bd94a7d3e9152fc03fe3
Olena <67316858+Olena-Zelenchuk@users.noreply.github.com>
2021-04-06 13:57:45 +0300

    додав розмітку перемикача тем

--- task_switch.js
+++ task_switch.js
@@ -1,44 +1,42 @@
 "use strict";
 
 const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548",
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
 ];
-
-// console.log(colors.indexOf("#FFFFFF"));
-
-const refs = {
-  body: document.querySelector("body"),
-  btnStart: document.querySelector('button[data-action="start"]'),
-  btnStop: document.querySelector('button[data-action="stop"]'),
-};
 
 let idSetInterval = null;
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-const changeColor = (array) => {
-  for (let i = 0; i < array.length; i += 1) {
-    refs.body.style.backgroundColor = array[randomIntegerFromInterval(0, i)];
-  }
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]')
+}
+
+refs.startBtn.addEventListener('click', onChangeBodyColor);
+refs.stopBtn.addEventListener('click', onStopChangeBodyColor);
+
+function randomIntegerFromInterval (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const onStartChangeColor = () => {
-  idSetInterval = setInterval(() => {
-    refs.btnStart.removeEventListener("click", onStartChangeColor);
-    changeColor(colors);
-  }, 1000);
+function onChangeBodyColor() {
+    
+idSetInterval = setInterval(() => {
+        const i = randomIntegerFromInterval(0, (colors.length - 1));
+        refs.body.style.backgroundColor = colors[i];
+        refs.startBtn.disabled = true;
+    }, 1000);
 };
 
-const onStopChangeColor = () => {
-  clearInterval(idSetInterval);
-  refs.btnStart.addEventListener("click", onStartChangeColor);
-};
+function onStopChangeBodyColor() {
+    clearInterval(idSetInterval);
+    refs.startBtn.disabled = false;
+}
 
-refs.btnStart.addEventListener("click", onStartChangeColor);
-refs.btnStop.addEventListener("click", onStopChangeColor);
+
+
